3b0b3ab07140fe912341f2ad5c19686e391905a3
Alvaro Ortega <alvaroortega@Alvaros-MacBook-Pro.local>
2025-02-04 22:48:57 +0100

    Add tsconfig and set node version for Render

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+  res.send({ hi: 'there' });
 });
 
 const PORT = process.env.PORT || 3999
